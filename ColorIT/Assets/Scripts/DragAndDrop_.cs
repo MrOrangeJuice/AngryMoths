7fbdb09aa1235cdd62d3d05be6e68639ff5d934c
Pr9269 <pr9269@g.rit.edu>
2021-12-01 19:05:45 -0500

    Update Jigsaw Puzzle

--- ColorIT/Assets/Scripts/DragAndDrop_.cs
+++ ColorIT/Assets/Scripts/DragAndDrop_.cs
@@ -6,16 +6,19 @@ using UnityEngine.SceneManagement;
 public class DragAndDrop_ : MonoBehaviour
 {
     public Sprite[] Levels;
-
     public GameObject EndMenu;
     public GameObject SelectedPiece;
     public GameObject winText;
     int OIL = 1;    
     public int PlacedPieces = 0;
+
+    private bool coroutineAllowed;
+
     void Start()
     {
         for (int i = 0;i < 36; i++)
         {
+            coroutineAllowed = true;
             GameObject.Find("Piece (" + i + ")").transform.Find("Puzzle").GetComponent<SpriteRenderer>().sprite = Levels[PlayerPrefs.GetInt("Level")];
         }
         
@@ -55,5 +58,42 @@ public class DragAndDrop_ : MonoBehaviour
         {
             winText.SetActive(true);
         }
+    }
+
+    private void OnMouseOver()
+    {
+        if(coroutineAllowed)
+        {
+            StartCoroutine("StartPulsating");
+        }
+    }
+
+    private IEnumerator StartPulsating()
+    {
+        coroutineAllowed = false;
+
+        for(float i = 0f; i <= 1f; i += 0.1f)
+        {
+            transform.localScale = new Vector3(
+            (Mathf.Lerp(transform.localScale.x, transform.localScale.x + 0.025f, Mathf.SmoothStep(0f,1f,i))),
+            (Mathf.Lerp(transform.localScale.y, transform.localScale.y + 0.025f, Mathf.SmoothStep(0f, 1f, i))),
+            (Mathf.Lerp(transform.localScale.z, transform.localScale.z + 0.025f, Mathf.SmoothStep(0f, 1f, i)))
+            );
+
+            yield return new WaitForSeconds(0.015f);
+        }
+
+        for (float i = 0f; i <= 1f; i += 0.1f)
+        {
+            transform.localScale = new Vector3(
+            (Mathf.Lerp(transform.localScale.x, transform.localScale.x - 0.025f, Mathf.SmoothStep(0f, 1f, i))),
+            (Mathf.Lerp(transform.localScale.y, transform.localScale.y - 0.025f, Mathf.SmoothStep(0f, 1f, i))),
+            (Mathf.Lerp(transform.localScale.z, transform.localScale.z - 0.025f, Mathf.SmoothStep(0f, 1f, i)))
+            );
+
+            yield return new WaitForSeconds(0.015f);
+        }
+
+        coroutineAllowed = true;
     }
 }
--- ColorIT/Assets/Scripts/piceseScript.cs
+++ ColorIT/Assets/Scripts/piceseScript.cs
@@ -5,11 +5,15 @@ using UnityEngine.Rendering;
 
 public class piceseScript : MonoBehaviour
 {
+    private bool coroutineAllowed;
+
     private Vector3 RightPosition;
     public bool InRightPosition;
     public bool Selected;
     void Start()
     {
+        coroutineAllowed = true;
+
         RightPosition = transform.position;
         // transform.position = new Vector3(Random.Range(5f, 9f), Random.Range(2.5f, -5));
         transform.position = new Vector3(Random.Range(6.5f, 11f), Random.Range(2.25f, -2.5f));
@@ -30,5 +34,42 @@ public class piceseScript : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void OnMouseOver()
+    {
+        if (coroutineAllowed)
+        {
+            StartCoroutine("StartPulsating");
+        }
+    }
+
+    private IEnumerator StartPulsating()
+    {
+        coroutineAllowed = false;
+
+        for (float i = 0f; i <= 1f; i += 0.1f)
+        {
+            transform.localScale = new Vector3(
+            (Mathf.Lerp(transform.localScale.x, transform.localScale.x + 0.017f, Mathf.SmoothStep(0f, 1f, i))),
+            (Mathf.Lerp(transform.localScale.y, transform.localScale.y + 0.017f, Mathf.SmoothStep(0f, 1f, i))),
+            (Mathf.Lerp(transform.localScale.z, transform.localScale.z + 0.017f, Mathf.SmoothStep(0f, 1f, i)))
+            );
+
+            yield return new WaitForSeconds(0.015f);
+        }
+
+        for (float i = 0f; i <= 1f; i += 0.1f)
+        {
+            transform.localScale = new Vector3(
+            (Mathf.Lerp(transform.localScale.x, transform.localScale.x - 0.017f, Mathf.SmoothStep(0f, 1f, i))),
+            (Mathf.Lerp(transform.localScale.y, transform.localScale.y - 0.017f, Mathf.SmoothStep(0f, 1f, i))),
+            (Mathf.Lerp(transform.localScale.z, transform.localScale.z - 0.017f, Mathf.SmoothStep(0f, 1f, i)))
+            );
+
+            yield return new WaitForSeconds(0.015f);
+        }
+
+        coroutineAllowed = true;
     }
 }
